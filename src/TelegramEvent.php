1692190c206dca00212e9dfce9bce1ae7412a121
Michael K. Laweh [ LAWEITECH ] <git@michael.laweitech.com>
2022-06-16 23:48:28 +0000

    Improved Telegram Message Content to remove ambiguity

--- src/TelegramEvent.php
+++ src/TelegramEvent.php
@@ -9,21 +9,45 @@ use LogicException;
 
 class TelegramEvent extends Event
 {
+    //inspiration:
+    //Illuminate\Console\Scheduling\Event
+    //https://github.com/illuminate/console/blob/025d017ac98976aa4c6346540c249c060b7b2883/Scheduling/Event.php
+    
+
+    /**
+     * Ensure that the command output is being captured.
+     *
+     * @return void
+     */
+    protected function ensureOutputIsBeingCaptured()
+    {
+        if (is_null($this->output) || $this->output == $this->getDefaultOutput()) {
+            $this->sendOutputTo(storage_path('logs/schedule-'.sha1($this->mutexName()).'.log'));
+        }
+    }
     
     public function telegramOutputTo($chatId) {
 
-        if (is_null($this->output) || $this->output == $this->getDefaultOutput()) {
-            throw new LogicException('Must direct output to a file in order to get telegram results.');
+        $this->ensureOutputIsBeingCaptured();
+
+        $text = is_file($this->output) ? file_get_contents($this->output) : '';
+
+        if (empty($text)) {
+            return;
         }
 
         return $this->then(function () use($chatId) {
-            $contents = "*" . $this->description . "*" . PHP_EOL;
-            $contents .= file_get_contents($this->output);
+
+            $contents = "*" . "Scheduled Job Output For [{$this->command}]" . "*" . PHP_EOL;
+            $contents .= "*" . $this->description . "*" . PHP_EOL;
+            $contents .= $text;
+
             Telegram::sendMessage([
                 'chat_id' => $chatId,
                 'text' => $contents,
                 'parse_mode' => 'Markdown'
             ]);
+            
         });
 
     }
